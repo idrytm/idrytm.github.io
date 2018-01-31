8e798340070c634baa7ee4c52ddf9ede5a1afdc2
Dmitry <idrytm@yandex.ru>
2018-02-01 02:22:50 +0400

    Rewrite css on sass

--- test-project/gulpfile.js
+++ test-project/gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require("gulp"),
 	sass = require("gulp-sass");
 
 gulp.task("sass", function() {
-	return gulp.src("app/sass/main.scss")
+	return gulp.src("app/sass/main.sass")
 		.pipe(sass({outputStyle: "expanded"}).on("error", sass.logError))
 		.pipe(gulp.dest("dist/css"));
 })	
 
 gulp.task("watch", function() {
-	gulp.watch("app/sass/main.scss", ["sass"]);
+	gulp.watch("app/sass/main.sass", ["sass"]);
 });
 
 gulp.task("default", ["watch"]);
